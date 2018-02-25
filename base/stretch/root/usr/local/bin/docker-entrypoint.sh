5804d435c7f1e2a7299ea5caf7198608178ad402
Sebastian Jastrzebski <sebby2k@gmail.com>
2018-02-24 21:29:50 -0500

    images: initial set of apps

---
+++ base/stretch/root/usr/local/bin/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+HOST_IPV4="$(hostname -i)"
+export HOST_IPV4
+echo "Host IP: ${HOST_IPV4}"
+
+if [ -n "${WAIT_FOR_SERVICE}" ]; then
+	SERVICE_IPS=""
+	while [ -z "${SERVICE_IPS}" ]; do
+			echo "Waiting for ${WAIT_FOR_SERVICE} ..."
+	    sleep 5
+	    SERVICE_IPS=$(getent hosts "${WAIT_FOR_SERVICE}" || echo "")
+	done
+fi 
+
+echo "Running confd ..."
+confd -onetime -backend env
+
+if [ "${1:0:1}" = '-' ]; then
+  set -- ${DAEMON} "$@"
+fi
+
+if [ "$1" = "${DAEMON}" -a "$(id -u)" = "0" ]; then
+  echo "Running chown $(pwd) ..."
+  chown -R ${DAEMON_USER} .
+  set -- gosu ${DAEMON_USER} "$@"
+fi
+
+echo "Launching: $@"
+exec "$@"
